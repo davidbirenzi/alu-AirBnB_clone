137389b41dbbe1b070b1916eca20f95801d1da76
davidbirenzi <d.birenzi@alustudent.com>
2025-01-10 20:13:01 +0200

    updated BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,62 @@
-#!/usr/bin/python3
+# !/usr/bin/python3
+"""
+Test cases for the BaseModel class.
+"""
+
+import unittest
 from models.base_model import BaseModel
 
-my_model = BaseModel()
-my_model.name = "My First Model"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+class TestBasemodel(unittest.TestCase):
+    """
+    Test cases for the BaseModel class.
+    """
+    def test_init(self):
+        """
+        Tests that a BaseModel instance is properly initialized with an id,
+        created_at and updated_at attributes.
+        """
+        my_model = BaseModel()
+        self.assertIsNotNone(my_model.id)
+        self.assertIsNotNone(my_model.created_at)
+        self.assertIsNotNone(my_model.updated_at)
+
+    def test_save(self):
+        """
+        Tests that the save method of a BaseModel instance updates the
+        updated_at attribute. 
+        """
+        my_model = BaseModel()
+        initial_updated_at = my_model.updated_at
+        my_model.save()
+        current_updated_at = my_model.updated_at
+        self.assertNotEqual(initial_updated_at, current_updated_at)
+
+    def test_to_dict(self):
+        """
+        Tests that the to_dict method of a BaseModel instance returns a
+        dictionary that includes the instance's attributes, its class name
+        and the current datetime for created_at and updated_at attributes.
+        """
+        my_model = BaseModel()
+        my_model_dict = my_model.to_dict()
+        self.assertIsInstance(my_model_dict, dict)
+        self.assertEqual(my_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(my_model_dict['id'], my_model.id)
+        self.assertEqual(my_model_dict['created_at'], my_model.created_at.isoformat())
+        self.assertEqual(my_model_dict['updated_at'], my_model.created_at.isoformat())
+
+    def test_str(self):
+        """
+        Tests that the __str__ method of a BaseModel instance returns a
+        string that is a valid representation of the instance. The string
+        should start with the class name in square brackets, and should
+        include the instance's id and to_dict() representation.
+        
+        """
+        my_model = BaseModel()
+        self.assertTrue(str(my_model).startswith("[BaseModel]"))
+        self.assertIn(my_model.id, str(my_model))
+        self.assertIn(str(my_model.to_dict()), str(my_model))
+
+if __name__ == '_main_':
+    unittest.main()
