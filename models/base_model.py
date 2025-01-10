d703e25a215b8e375595167c59a399175eaaf3bd
davidbirenzi <d.birenzi@alustudent.com>
2025-01-10 22:51:02 +0200

    Base model from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -8,11 +8,20 @@ from datetime import datetime
 class BaseModel:
     """Defines common attributes/methods for other classes."""
     
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize the BaseModel with unique ID, created_at, and updated_at."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    setattr(self, key, datetime.strptime(value, time_format))
+                elif key != "__class__":
+                    setattr(self, key, value)
+            
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """Return string representation of the BaseModel instance."""
