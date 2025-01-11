aec645f5b772418bfbc5ddb186f69b3eb86e7a37
cinna03 <c.mwiza1@alustudent.com>
2025-01-11 20:41:18 +0200

    Airbnb

--- console.py
+++ console.py
@@ -9,3 +9,17 @@ from models import storage
 
 # List of valid classes
 classes = {"BaseModel": BaseModel}
+
+class HBNBCommand(cmd.Cmd):
+    """Command interpreter for AirBnB clone."""
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit the program."""
+        return True
+
+    def do_EOF(self, arg):
+        """Exit the program with EOF signal."""
+        print()
+        return True
