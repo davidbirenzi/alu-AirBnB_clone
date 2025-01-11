4f849a00768c0ab53765f83b2199ae9813bbbc02
davidbirenzi <d.birenzi@alustudent.com>
2025-01-11 13:15:16 +0200

    added pass

--- console.py
+++ console.py
@@ -28,5 +28,6 @@ class HBNBCommand(cmd.Cmd):
         """
         Do nothing on an empty line or a line with only spaces + ENTER.
         """
+        pass
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
