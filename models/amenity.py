be6ce79dbf6c546e377d4370f21904cd5a2b5774
davidbirenzi <d.birenzi@alustudent.com>
2025-01-12 23:46:24 +0200

    number 9

--- models/amenity.py
+++ models/amenity.py
@@ -1,17 +1,8 @@
 #!/usr/bin/python3
 """Amenity class that inherits from BaseModel"""
-from os import getenv
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
+from models.base_model import BaseModel
 
-class Amenity(BaseModel, Base):
-    """ Amenity Class
-    Attributes:
-        name: input name
-    """
-    __tablename__ = 'amenities'
-    name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        place_amenities = relationship('Place', secondary='place_amenity', back_populates='amenities')
+class Amenity(BaseModel):
+    """Represents an Amenity"""
+    name = ""
